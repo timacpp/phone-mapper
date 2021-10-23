515e2d1b6d7d87e2d54822d2c5c79068f0057644
tima <timathevedmed@gmail.com>
2021-10-23 21:41:30 +0200

    Implement maptel_delete and create debug class

--- maptel.cc
+++ maptel.cc
@@ -9,16 +9,37 @@ namespace {
     using std::unordered_map;
 
     using telnum = string;
+    using dict_id = size_t;
     using dict = unordered_map<telnum, telnum>;
-    using dict_id = size_t; // TODO: Być może uint32_t lub size_t?
     using dictmap = unordered_map<dict_id, dict>;
 
+    class debug {
 #ifdef NDEBUG
-    constexpr auto debug{false};
+        static constexpr auto debug_compile{false};
 #else
-    constexpr auto debug{true};
+        static constexpr auto debug_compile{true};
 #endif
+    public:
 
+        template<typename... Args>
+        static void start_session(const string& func, Args const&... args) {
+            if (debug_compile) {
+                cerr << "maptel: " << func << '(';
+                ((cerr << args << ", "), ...);
+                cerr << ")\n";
+            }
+        }
+
+        template<typename... Args>
+        static void end_session(const string& func, Args const&... args) {
+            if (debug_compile) {
+                cerr << "maptel: " << func;
+                ((cerr << args << ' '), ...);
+                cerr << '\n';
+            }
+        }
+    };
+    
     dictmap& maptel() {
         static dictmap maptel;
         return maptel;
@@ -29,12 +50,9 @@ namespace {
         return dict_count;
     }
 
-    template<typename... Args>
-    void show_debug(const Args&... out) {
-        if (debug) {
-            cerr << "maptel: ";
-            (cerr << ... << out) << "\n";
-        }
+    dict& get_dict(unsigned long id) {
+        assert(maptel().count(id));
+        return maptel()[id];
     }
 
     bool telnum_validate(char const *tel) {
@@ -53,42 +71,43 @@ namespace {
         assert(telnum_validate(tel));
         return {tel};
     }
-
-    dict& get_dict(unsigned long id) {
-        assert(maptel().count(id));
-        return maptel()[id];
-    }
 }
 
 namespace jnp1 {
     unsigned long maptel_create(void) {
-        show_debug(__FUNCTION__, "()");
+        debug::start_session(__FUNCTION__);
 
         dict_id next_id{dict_count()++};
         maptel()[next_id] = dict();
 
         assert(maptel().count(next_id));
-        show_debug(__FUNCTION__, ": new map next_id = ", next_id);
+        debug::end_session(__FUNCTION__, ": new map id =", next_id);
 
         return next_id;
     }
 
     void maptel_delete(unsigned long id) {
+        debug::start_session(__FUNCTION__, id);
 
+        assert(maptel().count(id));
+        maptel().erase(id);
+
+        assert(!maptel().count(id));
+        debug::end_session(__FUNCTION__, "map", id, "deleted");
     }
 
     void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst) {
-        dict chosen_dict{get_dict(id)};
+        debug::start_session(__FUNCTION__, id, tel_src, tel_dst);
+
+        auto chosen_dict{get_dict(id)};
         const auto src{telnum_create(tel_src)};
         const auto dest{telnum_create(tel_dst)};
 
-        show_debug(__FUNCTION__, '(', id, ", ", src, ", ", dest, ')');
-
+        assert(!chosen_dict.count(src));
         chosen_dict[src] = dest;
 
         assert(chosen_dict.count(src) && chosen_dict[src] == dest);
-
-        show_debug(__FUNCTION__,  ": inserted");
+        debug::end_session(__FUNCTION__, ": inserted");
     }
 
     void maptel_erase(unsigned long id, char const *tel_src) {
