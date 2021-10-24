5655c1e4a36be30763e6fa50cf147a51a44be108
tima <timathevedmed@gmail.com>
2021-10-24 23:09:36 +0200

    Implement maptel_transform and fix extra separator bug

--- maptel.cc
+++ maptel.cc
@@ -1,12 +1,14 @@
 #include <cassert>
 #include <cstring>
+#include <unordered_set>
 #include <unordered_map>
 #include "maptel.h"
 
 namespace {
-    using std::cerr;
-    using std::string;
-    using std::unordered_map;
+    using std::cerr, std::endl;
+    using std::string, std::strcpy;
+    using std::pair, std::make_pair, std::tuple;
+    using std::unordered_set, std::unordered_map;
 
     using telnum = string;
     using dict_id = size_t;
@@ -19,26 +21,37 @@ namespace {
 #else
         static constexpr auto debug_compile{true};
 #endif
-        template<typename... Args>
-        static void print_behaviour(const string& pref, const string& sep,
-                                    const string& post, Args const&... args) {
-            cerr << "maptel: " << pref;
-            ((cerr << args << sep), ...);
-            cerr << post;
+
+        using strtup = tuple<string, string, string>;
+
+        template<typename Arg, typename... Args>
+        static void print_behaviour(const strtup& format, const Arg& first = "",
+                                    const Args&... args) {
+            const auto& [prefix, separator, postfix]{format};
+            ((cerr << "maptel: " << prefix << first) << ... <<
+                (cerr << separator, args)) << postfix << endl;
         }
 
     public:
-        template<typename... Args>
-        static void start_session(const string& func, Args const&... args) {
+        template<typename Arg, typename... Args>
+        static void start_session(const string& func, const Arg& first = "",
+                                  const Args&... args) {
             if (debug_compile) {
-                print_behaviour(func + '(', ", ", ")\n", args...);
+                print_behaviour({func + '(', ", ", ")"}, first, args...);
             }
         }
 
-        template<typename... Args>
-        static void end_session(const string& func, Args const&... args) {
+        template<typename Arg, typename... Args>
+        static void end_session(const string& func, const Arg& first,
+                                const Args&... args) {
             if (debug_compile) {
-                print_behaviour(func + ": ", " ", "\n", args...);
+                print_behaviour({func + ": ", " ", ""}, first, args...);
+            }
+        }
+
+        static void print_message(const string& func, const string& msg) {
+            if (debug_compile) {
+                print_behaviour({func + ": ", "", ""}, msg);
             }
         }
     };
@@ -74,11 +87,28 @@ namespace {
         assert(telnum_validate(tel));
         return {tel};
     }
+
+    pair<telnum&, bool> find_last_telnum(dict& from, telnum& src) {
+        telnum& last{src};
+        bool cycle_exists = false;
+        unordered_set<telnum> visited;
+
+        while (from.count(last) && !cycle_exists) {
+            cycle_exists |= visited.count(last);
+            visited.insert(last);
+            last = from[last];
+        }
+
+        if (cycle_exists)
+            last = src;
+
+        return {last, cycle_exists};
+    };
 }
 
 namespace jnp1 {
     unsigned long maptel_create(void) {
-        debug::start_session(__FUNCTION__);
+        debug::start_session(__FUNCTION__, "");
 
         dict_id next_id{dict_count()++};
         maptel()[next_id] = dict();
@@ -99,35 +129,49 @@ namespace jnp1 {
         debug::end_session(__FUNCTION__, "map", id, "deleted");
     }
 
-    void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst) {
-        debug::start_session(__FUNCTION__, id, tel_src, tel_dst);
-
+    void maptel_insert(unsigned long id, char const *tel_src,char const *tel_dst) {
         auto chosen_dict{get_dict(id)};
         const auto src{telnum_create(tel_src)};
         const auto dest{telnum_create(tel_dst)};
 
+        debug::start_session(__FUNCTION__, id, src, dest);
+
         assert(!chosen_dict.count(src));
         chosen_dict[src] = dest;
-
         assert(chosen_dict.count(src) && chosen_dict[src] == dest);
+
         debug::end_session(__FUNCTION__, "inserted");
     }
 
     void maptel_erase(unsigned long id, char const *tel_src) {
-        debug::start_session(__FUNCTION__ , id, tel_src);
-
         auto chosen_dict{get_dict(id)};
         const auto src{telnum_create(tel_src)};
-        const auto debug_res_msg{
-            chosen_dict.count(src) ? "erased" : "nothing to erase"
+        const auto debug_end_msg{
+                chosen_dict.count(src) ? "erased" : "nothing to erase"
         };
 
-        chosen_dict.erase(src);
+        debug::start_session(__FUNCTION__ , id, tel_src);
 
+        chosen_dict.erase(src);
         assert(!chosen_dict.count(src));
-        debug::end_session(__FUNCTION__, debug_res_msg);
+
+        debug::end_session(__FUNCTION__, debug_end_msg);
     }
 
     void maptel_transform(unsigned long id, char const *tel_src, char *tel_dst, size_t len) {
+        auto chosen_dict{get_dict(id)};
+        auto src{telnum_create(tel_src)};
+        const auto& [dest, cycle_exists]{find_last_telnum(chosen_dict, src)};
+
+        debug::start_session(__FUNCTION__, id, tel_src, (void *) tel_dst, len);
+
+        if (cycle_exists) {
+            debug::print_message(__FUNCTION__, "cycle detected");
+        }
+
+        assert(dest.size() < len);
+        strcpy(tel_dst, dest.c_str());
+
+        debug::end_session(__FUNCTION__, src, "->", tel_dst);
     }
 }
